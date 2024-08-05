3def171bde6acc6525061c221253d759060f3662
Gerson <gersonmurillo2711@gmail.com>
2024-08-05 08:43:34 -0600

    Primer commit

---
+++ android/app/src/main/kotlin/com/example/kardex/MainActivity.kt
@@ -0,0 +1,5 @@
+package unah.edu.hn.kardexinventario
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hola', style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.red,
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ),  
+      ),
+    );
+  }
+}
